c5a62660f6aa5d881ea34e0d80838c5aaa905c5e
Ronald Mannak <ronaldmannak@me.com>
2022-07-13 20:48:49 -0700

    Add documentation

--- Sources/MEWwalletTweetNacl/TweetNacl.swift
+++ Sources/MEWwalletTweetNacl/TweetNacl.swift
@@ -64,7 +64,16 @@ public enum TweetNaclError: LocalizedError {
   }
 }
 
+// Based on https://github.com/dchest/tweetnacl-js
 public class TweetNacl {
+  
+  // MARK: - Keys
+
+  /// Creates key pair on curve25519 as specified in EIP1024. Pass nil to create a new key pair or an Ethereum private key
+  /// to create a key pair linked to an Ethereum key pair.
+  /// Based on nacl.box.keyPair.fromSecretKey and nacl.box.keyPair
+  /// - Parameter fromSecretKey: Ethereum private key
+  /// - Returns: curve25519 key pair
   public static func keyPair(fromSecretKey: Data? = nil) throws -> (publicKey: Data, secretKey: Data) {
     var sk: [UInt8]
     if let fromSecretKey = fromSecretKey {
@@ -79,34 +88,53 @@ public class TweetNacl {
     }
           
     var pk = [UInt8](repeating: 0, count: Constants.PublicKeyLength)
-    
     let result = crypto_scalarmult_curve25519_tweet_base(&pk, &sk)
-    
     guard result == 0 else { throw TweetNaclError.tweetNacl("[TweetNacl.keyPair] Internal error code: \(result)") }
-    
     return (Data(pk), Data(sk))
-  }        
-  
+  }
+    
+  /// Pre-calculate shared secret key
+  /// Based on nacl.box.before
+  /// - Parameters:
+  ///   - publicKey: public key
+  ///   - secretKey: private key
+  /// - Returns: shared key
+  internal static func before(publicKey: Data, secretKey: Data) throws -> Data {
+    guard publicKey.count == Constants.PublicKeyLength else { throw TweetNaclError.invalidPublicKey }
+    guard secretKey.count == Constants.SecretKeyLength else { throw TweetNaclError.invalidSecretKey }
+      
+    var publicKey = [UInt8](publicKey)
+    var secretKey = [UInt8](secretKey)
+    var k = [UInt8](repeating: 0, count: Constants.BeforeNMLength)
+      
+    let result = crypto_box_curve25519xsalsa20poly1305_tweet_beforenm(&k, &publicKey, &secretKey)
+    guard result == 0 else { throw TweetNaclError.tweetNacl("[TweetNacl.before] Internal error code: \(result)") }
+      
+    return Data(k)
+  }
+    
+  // MARK: - Decryption
+    
+  /// Decrypts encrypted message
+  /// Based on nacl.box.open
+  /// - Parameters:
+  ///   - message: secret box
+  ///   - nonce: unique nonce
+  ///   - publicKey: public curve25519 key provided by sender
+  ///   - secretKey: receiver's private curve25519 key to decrypt message
+  /// - Returns: clear text
   public static func open(message: Data, nonce: Data, publicKey: Data, secretKey: Data) throws -> Data {
     let k = try before(publicKey: publicKey, secretKey: secretKey)
     return try open(box: message, nonce: nonce, key: k)
   }
   
-  public static func before(publicKey: Data, secretKey: Data) throws -> Data {
-    guard publicKey.count == Constants.PublicKeyLength else { throw TweetNaclError.invalidPublicKey }
-    guard secretKey.count == Constants.SecretKeyLength else { throw TweetNaclError.invalidSecretKey }
-    
-    var publicKey = [UInt8](publicKey)
-    var secretKey = [UInt8](secretKey)
-    
-    var k = [UInt8](repeating: 0, count: Constants.BeforeNMLength)
-    
-    let result = crypto_box_curve25519xsalsa20poly1305_tweet_beforenm(&k, &publicKey, &secretKey)
-    guard result == 0 else { throw TweetNaclError.tweetNacl("[TweetNacl.before] Internal error code: \(result)") }
-    
-    return Data(k)
-  }
-  
+  /// Decrypts encrypted message
+  /// Based on nacl.secretbox.open
+  /// - Parameters:
+  ///   - box: secret box
+  ///   - nonce: unique nonce
+  ///   - key: private key
+  /// - Returns: clear text of message
   public static func open(box: Data, nonce: Data, key: Data) throws -> Data {
     guard key.count == Constants.SecretBox.keyLength else { throw TweetNaclError.invalidKey }
     guard nonce.count == Constants.SecretBox.nonceLength else { throw TweetNaclError.invalidNonce }
@@ -124,28 +152,44 @@ public class TweetNacl {
     
     return Data(m[Constants.SecretBox.zeroLength..<c.count])
   }
-  
-  public static func box(message: String, theirPublicKey: Data, mySecretKey: Data, nonce: Data? = nil) throws -> Data {
-    let k = try before(publicKey: theirPublicKey, secretKey: mySecretKey)
-    guard let data = message.data(using: .utf8) else { throw TweetNaclError.tweetNacl("Invalid message")}
-    return try secretbox(message: data, nonce: nonce ?? randomNonce(), key: k)
+    
+  // MARK: - Encryption
+    
+  /// Encrypts message, creates secretbox
+  /// Based on nacl.box
+  /// - Parameters:
+  ///   - message: Clear text message
+  ///   - theirPublicKey: Recipient's public key
+  ///   - mySecretKey: Sender's private key
+  ///   - nonce: nonce (pass nil for random nonce)
+  /// - Returns: secret box
+  public static func box(message: Data, recipientPublicKey: Data, senderSecretKey: Data, nonce: Data? = nil) throws -> Data {
+    let k = try before(publicKey: recipientPublicKey, secretKey: senderSecretKey)
+    return try secretbox(message: message, nonce: nonce ?? randomNonce(), key: k)
   }
-
+    
+  /// Encrypts message, creates secretbox
+  /// Based on nacl.secretbox
+  /// - Parameters:
+  ///   - message: Clear text message
+  ///   - nonce: Unique nonce
+  ///   - key: Shared secret key
+  /// - Returns: secret box
   private static func secretbox(message: Data, nonce: Data, key: Data) throws -> Data {
     guard key.count == Constants.SecretBox.keyLength else { throw TweetNaclError.invalidKey }
     guard nonce.count == Constants.SecretBox.nonceLength else { throw TweetNaclError.invalidNonce }
       
-    var mData = Data(count: message.count + Constants.SecretBox.zeroLength)
-    mData.replaceSubrange(Constants.SecretBox.boxZeroLength..<mData.count, with: message)
-    let m = [UInt8](mData)
-    var c = [UInt8](repeating: 0, count: mData.count)
+    var mData = Data(count: Constants.SecretBox.zeroLength + message.count)
+    mData.replaceSubrange(Constants.SecretBox.boxZeroLength ..< message.count, with: message)
+    var m = [UInt8](mData)
+    var c = [UInt8](repeating: 0, count: m.count)
     var nonce = [UInt8](nonce)
     var key = [UInt8](key)
       
-    let result = crypto_secretbox_xsalsa20poly1305_tweet(&c, m, UInt64(m.count), &nonce, &key)
+    let result = crypto_secretbox_xsalsa20poly1305_tweet(&c, &m, UInt64(m.count), &nonce, &key)
     guard result == 0 else { throw TweetNaclError.tweetNacl("[TweetNacl.secretbox] Internal error code: \(result)") }
     
-    return Data(c[Constants.SecretBox.boxZeroLength..<c.count])
+      return Data(c[Constants.SecretBox.boxZeroLength..<c.count])
   }
     
   private static func randomNonce() throws -> Data {
