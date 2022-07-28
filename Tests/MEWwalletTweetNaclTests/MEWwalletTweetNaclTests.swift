8a6fdc5a5e3e6528047159392e57f80966e30f75
Ronald Mannak <ronaldmannak@me.com>
2022-07-28 11:55:19 -0700

    Code cleanup unit tests

--- Tests/MEWwalletTweetNaclTests/MEWwalletTweetNaclTests.swift
+++ Tests/MEWwalletTweetNaclTests/MEWwalletTweetNaclTests.swift
@@ -33,36 +33,22 @@ final class MEWwalletTweetNaclTests: XCTestCase {
     }
   }
     
-  /*
-    Tope's example:
-    getEncryptionPublicKey(7e5374ec2ef0d91761a6e72fdf8f6ac665519bfdf6da0a2329cf0d804514b816) should return a public encryption key of the form "C5YMNdqE4kLgxQhJO1MfuQcHP5hjVSXzamzd/TxlR0U="
-    
-    web3.eth.encrypt("C5YMNdqE4kLgxQhJO1MfuQcHP5hjVSXzamzd/TxlR0U=", 'x25519-xsalsa20-poly1305-v1', {data: 'My name is Satoshi Buterin'}) should return a blob of the form { version: 'x25519-xsalsa20-poly1305', nonce: '1dvWO7uOnBnO7iNDJ9kO9pTasLuKNlej', ephemPublicKey: 'FBH1/pAEHOOW14Lu3FWkgV3qOEcuL78Zy+qW1RwzMXQ=', ciphertext: 'f8kBcl/NCyf3sybfbwAKk/np2Bzt9lRVkZejr6uh5FgnNlH/ic62DZzy' }
-    
-    web3.eth.decrypt('7e5374ec2ef0d91761a6e72fdf8f6ac665519bfdf6da0a2329cf0d804514b816', { version: 'x25519-xsalsa20-poly1305', nonce: '1dvWO7uOnBnO7iNDJ9kO9pTasLuKNlej', ephemPublicKey: 'FBH1/pAEHOOW14Lu3FWkgV3qOEcuL78Zy+qW1RwzMXQ=', ciphertext: 'f8kBcl/NCyf3sybfbwAKk/np2Bzt9lRVkZejr6uh5FgnNlH/ic62DZzy' }) should return plain text/file of the form { data:'My name is Satoshi Buterin' }
-  */
-    
   func testEncode() throws {
     let receiverKeys = try TweetNacl.keyPair(fromSecretKey: privateKey)
-//    let ephemPublicKeyData = Data(base64Encoded: self.ephemPublicKey)!
     let ephemKeys = try TweetNacl.keyPair()
     let message = "My name is Satoshi Buterin".data(using: .utf8)!
     let nonceData = Data(base64Encoded: self.nonce)!
     
     XCTAssertEqual(String(data: receiverKeys.publicKey.base64EncodedData(), encoding: .utf8), "C5YMNdqE4kLgxQhJO1MfuQcHP5hjVSXzamzd/TxlR0U=")
-//    XCTAssertEqual(String(data: ephemPublicKeyData.base64EncodedData(), encoding: .utf8), "FBH1/pAEHOOW14Lu3FWkgV3qOEcuL78Zy+qW1RwzMXQ=")
     XCTAssertEqual(String(data: nonceData.base64EncodedData(), encoding: .utf8), "1dvWO7uOnBnO7iNDJ9kO9pTasLuKNlej")
       
     // encrypt
-//    let secretbox = try TweetNacl.box(message: message, recipientPublicKey: receiverKeys.publicKey, senderSecretKey: ephemPublicKeyData, nonce: nonceData)
     let secretbox = try TweetNacl.box(message: message, recipientPublicKey: receiverKeys.publicKey, senderSecretKey: ephemKeys.secretKey, nonce: nonceData)
     let secretboxString = String(data: secretbox.box.base64EncodedData(), encoding: .utf8)!
     let expectedCiphertext = "f8kBcl/NCyf3sybfbwAKk/np2Bzt9lRVkZejr6uh5FgnNlH/ic62DZzy"
     XCTAssertEqual(secretboxString.count, expectedCiphertext.count)
-//    XCTAssertEqual(secretboxString, expectedCiphertext)
     
     // decrypt
-//    let decrypted = try TweetNacl.open(message: secretbox, nonce: nonceData, publicKey: ephemPublicKeyData, secretKey: receiverKeys.secretKey)
     let decrypted = try TweetNacl.open(message: secretbox.box, nonce: nonceData, publicKey: ephemKeys.publicKey, secretKey: receiverKeys.secretKey)
     guard let decryptedMessage = String(data: decrypted, encoding: .utf8) else {
       XCTFail("Can't create a string")
